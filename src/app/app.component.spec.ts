e1e66d93740d47278434b498c3800948db4e1d6f
TojoNoum <tojonomenjanahary98@gmail.com>
2022-03-26 21:18:25 +0545

    modification de padding p-4 en p-md-4 dans le tamplate de landingPageComponent linge 10

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'YesApp'`, () => {
+  it(`should have as title 'YesGoPlat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('YesApp');
+    expect(app.title).toEqual('YesGoPlat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('YesApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('YesGoPlat app is running!');
   });
 });
